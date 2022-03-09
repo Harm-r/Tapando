3000ae58c45bc20e8710451cfecea43e1f9ebdca
Thomas Luijkman <thomasluijkman@gmail.com>
2022-03-09 16:20:26 +0100

    completed zoom script, added test

--- Assets/zoom.cs
+++ Assets/zoom.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+/* ZOOM SCRIPT
+* Used to detect pinch movements on a touch screen.
+* Zooms in and out on a 3D model in the 2D UI.
+* 
+* To use, attach to any camera-object with orthographic projection pointing at 3D model.
+*/
+
 public class zoom : MonoBehaviour{
-    // Start is called before the first frame update
     public float zoomFactor = 0.1f;
     public float minZoom = 1;
     public float maxZoom = 8;
     private Camera cam;
+    
+    // variables used for testing automatic zooming
+    // TODO: remove below before final handin
     public bool test = true;
     private float test_zoom;
+    // TODO: remove above before final handin
 
+    // Start is called before the first frame update
     void Start(){
         cam = this.gameObject.GetComponent<Camera>();
     }
@@ -19,9 +30,11 @@ public class zoom : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         if (Input.touchCount == 2 && !test){
+            // get touch inputs
             Touch fingerOne = Input.GetTouch(0);
             Touch fingerTwo = Input.GetTouch(1);
 
+            // get difference of distance between fingers
             float prevDistance = (getPrevPos(fingerOne) - getPrevPos(fingerTwo)).magnitude;
             float distance = (fingerOne.position - fingerTwo.position).magnitude;
             float diff = distance - prevDistance;
@@ -29,13 +42,14 @@ public class zoom : MonoBehaviour{
             perform_zoom(diff * zoomFactor);
         }
         // automatically zooms in and out to test limits on PC
-        // TODO: remove case before final handin
+        // TODO: remove below before final handin
         else if (test){
             test_zoom = zoomFactor;
             if (((int) Time.time) % 10 == 0) test_zoom = -test_zoom;
             perform_zoom(test_zoom);
             Debug.Log("Orthographic size: " + cam.orthographicSize);
         }
+        // TODO: remove above before final handin
     }
 
     Vector2 getPrevPos(Touch touch){
