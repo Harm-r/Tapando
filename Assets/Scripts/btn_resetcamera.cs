adfb79ea78270559843c68180c52555c88dda953
Thomas Luijkman <thomasluijkman@gmail.com>
2022-03-17 13:50:06 +0100

    first attempt at button

---
+++ Assets/Scripts/btn_resetcamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btn_resetcamera : MonoBehaviour
+{
+    public Button button;
+    public Camera cam;
+    public Transform foot;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        cam = GameObject.Find("3dObjectCamera").GetComponent<Camera>();
+        foot = GameObject.Find("Foot_Short").GetComponent<Transform>();
+        btn.onClick.AddListener(ResetCamera);
+    }
+    
+    void ResetCamera(){
+    	cam.orthographicSize = 5;
+    	foot.Rotate(0,0,0);
+    }
+}
