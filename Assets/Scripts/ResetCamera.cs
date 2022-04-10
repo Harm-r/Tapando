f5948c332489c01f7811ffac82916959890f6e75
Thomas <thomasluijkman@gmail.com>
2022-04-10 19:45:43 +0200

    restructured camera reset script for easier testing

--- Assets/Scripts/ResetCamera.cs
+++ Assets/Scripts/ResetCamera.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class btn_resetcamera : MonoBehaviour
+/** RESET CAMERA FUNCTION
+ *  Script that finds the model and camera in a model scene, and resets them to their default position.
+ *  To use, add script to button component of GameObject.
+ */
+public class ResetCamera : MonoBehaviour
 {
     public Button button;
     public Camera cam;
     public Transform foot;
     
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        // Get components
         Button btn = button.GetComponent<Button>();
         cam = GameObject.Find("3dObjectCamera").GetComponent<Camera>();
         foot = GameObject.Find("Foot_Short").GetComponent<Transform>();
-        btn.onClick.AddListener(ResetCamera);
+        
+        // Add listener
+        // Done with lambda expression for easier testing
+        btn.onClick.AddListener(() => CameraReset(cam, foot));
     }
     
-    void ResetCamera(){
+    public void CameraReset(Camera cam, Transform foot){
     	cam.orthographicSize = 5;
     	foot.rotation = Quaternion.identity;
     }
