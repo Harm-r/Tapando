a5c42fa93a547b03fd6123812bb3518f5d611ee9
Leah122 <65678032+Leah122@users.noreply.github.com>
2022-04-25 17:15:33 +0200

    Finished animation for instructions
    -

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScript : MonoBehaviour
+{
+    public GameObject Instructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowHideInstructions() {
+        Debug.Log("Test");
+        if (Instructions != null)
+        {
+            Animator animator = Instructions.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool Show = animator.GetBool("Show");
+                animator.SetBool("Show", !Show);
+            }
+        }
+    }
+}
