d574ff1ec3c63838bbc45fa28fe04e384d32c292
ddegrauw <ddegrauw@outlook.com>
2022-05-25 16:25:50 +0200

    working camera reset

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -12,6 +12,28 @@ public class CameraPosition : MonoBehaviour
 
    public Vector3 GivePosition()
     {
-        return Vector3.left;
+       Vector3 currentScale = GameObject.Find("FootModel").GetComponent<Transform>().localScale;
+       Vector3 rotationToReturn;
+
+        //check the which angle must be given and return that vector
+        switch (CameraFocus)
+        {
+            case Angle.Left:
+                rotationToReturn = new Vector3(0, 90, 0);
+                break;
+            case Angle.Right:
+                rotationToReturn = new Vector3(0, 270, 0);
+                break;
+            default:
+                rotationToReturn = new Vector3(0, 0, 0);
+                break;
+
+        }
+        //if the foot is currently inversed, rotate the y axis
+        if (currentScale.x < 0)
+        {
+            return new Vector3(rotationToReturn.x, -rotationToReturn.y, rotationToReturn.z);
+        }
+        return rotationToReturn;
     }
 }
--- Assets/Scripts/ResetCamera.cs
+++ Assets/Scripts/ResetCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 /** RESET CAMERA FUNCTION
  *  Script that finds the model and camera in a model scene, and resets them to their default position.
@@ -10,8 +11,8 @@ using UnityEngine.UI;
 public class ResetCamera : MonoBehaviour
 {
     public Button button;
-    public Camera cam;
-    public Transform foot;
+    private Camera cam;
+    private GameObject foot;
     
     // Start is called before the first frame update
     public void Start()
@@ -19,16 +20,35 @@ public class ResetCamera : MonoBehaviour
         // Get components
         Button btn = button.GetComponent<Button>();
         cam = GameObject.Find("3dObjectCamera").GetComponent<Camera>();
-        foot = GameObject.Find("FootModel").GetComponent<Transform>();
-        
-        // Add listener
+        foot = GameObject.Find("FootModel");
+       
         // Done with lambda expression for easier testing
         btn.onClick.AddListener(() => CameraReset(cam, foot));
     }
     
-    public void CameraReset(Camera cam, Transform foot){
-    	cam.orthographicSize = 10;
-        foot.rotation = Quaternion.Euler(new Vector3(0,90,0));
+    public void CameraReset(Camera cam, GameObject foot){
+        //Find the scrollfoot component to use its functions and get the transform component of the foot
+        ScrollFoot m_scrollfoot = GameObject.FindObjectOfType(typeof(ScrollFoot)) as ScrollFoot;
+        Transform footTransform = foot.GetComponent<Transform>();
+        cam.orthographicSize = 8;
+        string currentStep = "";
+
+        //get all the steps currently in view, if none currentstep is the empty foot
+        List<string> stepsTaken = m_scrollfoot.GetCurrentSteps(m_scrollfoot.GetStepNames(), foot);
+        if (stepsTaken.Count != 0)
+        {
+              currentStep = stepsTaken.Last();
+        }
+
+        else
+
+        {
+              currentStep = "Foot";
+        }
+
+        //Rotate the foot to the position found on the current tape step
+        Vector3 transformVector = foot.transform.Find(currentStep).gameObject.GetComponent<CameraPosition>().GivePosition();
+        footTransform.rotation = Quaternion.Euler(transformVector);
         
     }
 }
